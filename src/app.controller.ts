573105e3d33b5fc3d46a62e00821b5a6a020d7a0
secretm <secretm224@gmail.com>
2025-01-24 16:25:36 +0900

    app controller 수정

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,18 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Res } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Response } from 'express';
+import { join } from 'path';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get("/")
+  // getHello(): string {
+  //   return this.appService.getHello();
+  // }
+  getYummyMap(@Res() res: Response) {
+    // yummymap.html 파일 반환
+    res.sendFile(join(__dirname, '..', 'public', 'yummymap.html'));
   }
 }
