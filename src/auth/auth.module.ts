eaf6ea62b233fbc98557765239d888db7e281d80
secretm <secretm224@gmail.com>
2025-02-19 22:53:21 +0900

    merge

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,8 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from '../entities/user.entity';
+import { UserRepository } from './user.repository';
 
 @Module({
-  providers: [AuthService],
-  exports:[AuthService]
+  imports: [TypeOrmModule.forFeature([User])], // UserRepository가 아닌 User Entity를 등록
+  providers: [AuthService,UserRepository],
+  exports:[AuthService,UserRepository]
+
 })
-export class AuthModule {}
+export class AuthModule {
+
+}
