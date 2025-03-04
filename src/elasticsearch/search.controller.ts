db2adc497d1efe9ff679196d0108ffa971816f67
seung hwan shin <67136763+sexyseunghwan@users.noreply.github.com>
2025-03-04 17:56:20 +0900

    검색엔진 api 구축
    검색엔진 api 구축

--- src/elasticsearch/search.controller.ts
+++ src/elasticsearch/search.controller.ts
@@ -1,6 +1,5 @@
 import { Controller, Get, Post, Body, Query, Param ,Headers} from '@nestjs/common';
 import { SearchService } from './search.service';
-import { StoreDto } from './dto/store.dto/store.dto';
 import { StoreSearch } from 'src/entities/store_search.entity';
 
 @Controller('search')
@@ -45,12 +44,25 @@ export class SearchController {
 	@Get('allData')
 	async getAllStores(@Headers('yummy-key') apikey: string): Promise<StoreSearch[]> {
 		
-		
-
 		if (!apikey || apikey !== process?.env?.YUMMY_API_KEY) {
 			throw new Error('yummy-key is invalid');
 		}
 
 		return this.searchService.searchAll<StoreSearch>('yummy-index', StoreSearch);
 	}
+
+	
+	/* 테스트 코드 -> 검색결과 API*/
+	@Get('searchStoreDatas')
+	async getSearchStoreDatas(
+		@Query('searchData') searchData: string
+	): Promise<StoreSearch[]> {
+
+		/* 01. 문자열 파싱 */
+		searchData = this.searchService.stringTranformationFunction(searchData);
+		console.log(searchData);
+
+		return this.searchService.searchStoreDate<StoreSearch>('yummy-index', searchData, StoreSearch);
+	}
+	
 }
--- src/elasticsearch/search.service.ts
+++ src/elasticsearch/search.service.ts
@@ -7,7 +7,7 @@ export class SearchService {
     constructor(private readonly elasticService: ElasticsearchService) {}
 
     /*
-        특정 인덱스의 모든 데이터를 가져오는 함수 -> Elasticsearch 특성상 한번에 10,000개만 가져올 수 있으므로 주의.
+        특정 인덱스의 모든 데이터를 가져오는 함수
     */
     async searchAll<T>(index: string, model: new (data: any) => T): Promise<T[]> {
         const result: SearchResponse<any> = await this.elasticService.search({
@@ -19,6 +19,90 @@ export class SearchService {
                 size: 10000
             }
         });
+
+        return result.hits.hits.map(hit => new model({
+            ...hit._source, 
+            timestamp: hit._source.timestamp ? new Date(hit._source.timestamp) : undefined
+        }));
+    }
+
+    /* 문자열 변환 함수 */
+    stringTranformationFunction(searchData: string): string {
+
+        /* 한글 초성 중성 종성 분리 */
+        const CHO = "ㄱㄲㄴㄷㄸㄹㅁㅂㅃㅅㅆㅇㅈㅉㅊㅋㅌㅍㅎ".split("");
+        const JUNG = "ㅏㅐㅑㅒㅓㅔㅕㅖㅗㅘㅙㅚㅛㅜㅝㅞㅟㅠㅡㅢㅣ".split("");
+        const JONG = ["", "ㄱ", "ㄲ", "ㄳ", "ㄴ", "ㄵ", "ㄶ", "ㄷ", "ㄹ", "ㄺ", "ㄻ", "ㄼ", "ㄽ", "ㄾ", "ㄿ", "ㅀ", "ㅁ", "ㅂ", "ㅄ", "ㅅ", "ㅆ", "ㅇ", "ㅈ", "ㅊ", "ㅋ", "ㅌ", "ㅍ", "ㅎ"];
+
+        // **겹받침 조합 규칙 (종성이 두 개 연속될 경우)**
+        const DOUBLE_JONG_MAP: { [key: string]: string } = {
+            "ㄱㅅ": "ㄳ", "ㄴㅈ": "ㄵ", "ㄴㅎ": "ㄶ", "ㄹㄱ": "ㄺ",
+            "ㄹㅁ": "ㄻ", "ㄹㅂ": "ㄼ", "ㄹㅅ": "ㄽ", "ㄹㅌ": "ㄾ",
+            "ㄹㅍ": "ㄿ", "ㄹㅎ": "ㅀ", "ㅂㅅ": "ㅄ"
+        };
+
+        const ENG_TO_KOR_MAP: { [key: string]: string } = {
+            'q': 'ㅂ', 'w': 'ㅈ', 'e': 'ㄷ', 'r': 'ㄱ', 't': 'ㅅ', 'y': 'ㅛ', 'u': 'ㅕ', 'i': 'ㅑ', 'o': 'ㅐ', 'p': 'ㅔ',
+            'a': 'ㅁ', 's': 'ㄴ', 'd': 'ㅇ', 'f': 'ㄹ', 'g': 'ㅎ', 'h': 'ㅗ', 'j': 'ㅓ', 'k': 'ㅏ', 'l': 'ㅣ',
+            'z': 'ㅋ', 'x': 'ㅌ', 'c': 'ㅊ', 'v': 'ㅍ', 'b': 'ㅠ', 'n': 'ㅜ', 'm': 'ㅡ'
+          };
+        
+        /* 특수문자 제거 */ 
+        const sanitizedData = searchData.replace(/[^a-zA-Z0-9가-힣\s]/g, '');
+        const convertedChars = searchData.split('').map(char => ENG_TO_KOR_MAP[char] || char);
+        
+        console.log("convertedChars: " + convertedChars);
+    
+        let result = "";
+        let buffer: string[] = [];
+
+        for (let char of convertedChars) {
+            buffer.push(char);
+            console.log("len = " + buffer.length);
+
+            if (buffer.length >= 2 && CHO.includes(buffer[0]) && JUNG.includes(buffer[1])) {
+                let choIndex = CHO.indexOf(buffer[0]);
+                let jungIndex = JUNG.indexOf(buffer[1]);
+                let jongIndex = 0; // 초기에 종성 존재하지 않음.
+
+                if (buffer.length >= 3 && JONG.includes(buffer[2])) {
+                    // 종성이 있을 경우
+                    jongIndex = JONG.indexOf(buffer[2]);
+
+                    if (jongIndex == -1) {
+                        jongIndex = 0;
+                        let completeChar = String.fromCharCode(0xAC00 + (choIndex * 21 + jungIndex) * 28 + jongIndex);
+                            
+                        console.log("completeChar: " + completeChar);
+                        result += completeChar;
+                    }
+                }
+            }
+        }
+
+        //let res = result + buffer.join('');
+        console.log("result: " + result);
+
+        // 의미없는 단어 한글변환
+        //const convEngToKor = searchData.split('').map(char => ENG_TO_KOR_MAP[char] || char).join('');
+
+        //console.log(convEngToKor)
+        
+        return sanitizedData;
+    }
+
+
+    async searchStoreDate<T>(index: string, searchData: string, model: new (data: any) => T): Promise<T[]> {
+        const result: SearchResponse<any> = await this.elasticService.search({
+            index: index,
+            body: {
+                query: {
+                    
+                },  
+                size: 10000
+            }
+        }); 
+
 
         return result.hits.hits.map(hit => new model({
             ...hit._source, 
