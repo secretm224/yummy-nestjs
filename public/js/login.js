e908ccbe2ce1f7dba047aa7155709991b92172ac
secretm <secretm224@gmail.com>
2025-03-12 22:51:20 +0900

    search address layer

--- public/js/login.js
+++ public/js/login.js
@@ -133,14 +133,119 @@ function loginWithKakao() {
     }
   }
 
-  function SearchAreaToggle() {
-    document.getElementById('search_addr_area').style.display = 'none';
-   // document.getElementById('search_addr_area')
-  }
+//   function SearchAreaToggle() {
+//     document.getElementById('search_addr_area').style.display = 'none';
+//    // document.getElementById('search_addr_area')
+//   }
 
-  function SearchAddress() {
-    // 현재 scroll 위치를 저장해놓는다.
-    var currentScroll = Math.max(document.body.scrollTop, document.documentElement.scrollTop);
+//   function SearchAddress() {
+//     // 현재 scroll 위치를 저장해놓는다.
+//     var currentScroll = Math.max(document.body.scrollTop, document.documentElement.scrollTop);
+//     new daum.Postcode({
+//         oncomplete: function(data) {
+//             // 검색결과 항목을 클릭했을때 실행할 코드를 작성하는 부분.
+
+//             // 각 주소의 노출 규칙에 따라 주소를 조합한다.
+//             // 내려오는 변수가 값이 없는 경우엔 공백('')값을 가지므로, 이를 참고하여 분기 한다.
+//             var addr = ''; // 주소 변수
+//             var extraAddr = ''; // 참고항목 변수
+
+//             //사용자가 선택한 주소 타입에 따라 해당 주소 값을 가져온다.
+//             if (data.userSelectedType === 'R') { // 사용자가 도로명 주소를 선택했을 경우
+//                 addr = data.roadAddress;
+//             } else { // 사용자가 지번 주소를 선택했을 경우(J)
+//                 addr = data.jibunAddress;
+//             }
+
+//             // 사용자가 선택한 주소가 도로명 타입일때 참고항목을 조합한다.
+//             if(data.userSelectedType === 'R'){
+//                 // 법정동명이 있을 경우 추가한다. (법정리는 제외)
+//                 // 법정동의 경우 마지막 문자가 "동/로/가"로 끝난다.
+//                 if(data.bname !== '' && /[동|로|가]$/g.test(data.bname)){
+//                     extraAddr += data.bname;
+//                 }
+//                 // 건물명이 있고, 공동주택일 경우 추가한다.
+//                 if(data.buildingName !== '' && data.apartment === 'Y'){
+//                     extraAddr += (extraAddr !== '' ? ', ' + data.buildingName : data.buildingName);
+//                 }
+//                 // 표시할 참고항목이 있을 경우, 괄호까지 추가한 최종 문자열을 만든다.
+//                 if(extraAddr !== ''){
+//                     extraAddr = ' (' + extraAddr + ')';
+//                 }
+//                 // 조합된 참고항목을 해당 필드에 넣는다.
+//                 //document.getElementById("sample3_extraAddress").value = extraAddr;
+            
+//             } else {
+//                 //document.getElementById("sample3_extraAddress").value = '';
+//             }
+
+//             // console.log(`addr=${addr}`);
+//             // console.log(`extraAddr=${extraAddr}`);
+
+//             // 우편번호와 주소 정보를 해당 필드에 넣는다.
+//             // document.getElementById('sample3_postcode').value = data.zonecode;
+//             // document.getElementById("sample3_address").value = addr;
+//             // // 커서를 상세주소 필드로 이동한다.
+//             // document.getElementById("sample3_detailAddress").focus();
+//                document.getElementById("address").value = addr;
+//             // iframe을 넣은 element를 안보이게 한다.
+//             // (autoClose:false 기능을 이용한다면, 아래 코드를 제거해야 화면에서 사라지지 않는다.)
+//             document.getElementById('search_addr_area').style.display = 'none';
+
+//             // 우편번호 찾기 화면이 보이기 이전으로 scroll 위치를 되돌린다.
+//             document.body.scrollTop = currentScroll;
+//         },
+//         // 우편번호 찾기 화면 크기가 조정되었을때 실행할 코드를 작성하는 부분. iframe을 넣은 element의 높이값을 조정한다.
+//         onresize : function(size) {
+//             document.getElementById('search_addr_area').style.height = size.height+'px';
+//         },
+//         // width : '100%',
+//         width:'350px',
+//         height : '100%',
+//         maxSuggestItems : 5
+//     }).embed(document.getElementById('search_addr_area'));
+
+//     // iframe을 넣은 element를 보이게 한다.
+//     document.getElementById('search_addr_area').style.display = 'block';
+// }
+
+function RegisterAddress(){
+
+    const address_type = document.querySelector("input[name='address-type']:checked");
+    const address_type_value = address_type ? address_type.value : null
+    const address = document.getElementById("address").value;
+    const user_name = window.env.login_user.name;
+
+    if(!user_name){
+        alert('로그인 이후 사용해주세요');
+        return;
+    }
+
+    if(!address_type_value){
+        alert('주소타입을 선택해주세요');
+        return;
+    }
+
+    if(!address){
+        alert('주소를 입력해 주세요');
+        return;
+    }
+
+    const address_json = {
+                            user_name:user_name,
+                            address_type:address_type_value,
+                            address:address
+                         }
+
+    console.log(address_json);                         
+}
+
+function SearchLayerToggle() {
+    // iframe을 넣은 element를 안보이게 한다.
+    document.getElementById('layer').style.display = 'none';
+}
+
+function SearchAddressLayer() {
     new daum.Postcode({
         oncomplete: function(data) {
             // 검색결과 항목을 클릭했을때 실행할 코드를 작성하는 부분.
@@ -173,69 +278,47 @@ function loginWithKakao() {
                     extraAddr = ' (' + extraAddr + ')';
                 }
                 // 조합된 참고항목을 해당 필드에 넣는다.
-                //document.getElementById("sample3_extraAddress").value = extraAddr;
+                //document.getElementById("sample2_extraAddress").value = extraAddr;
             
             } else {
-                //document.getElementById("sample3_extraAddress").value = '';
+                //document.getElementById("sample2_extraAddress").value = '';
             }
 
-            // console.log(`addr=${addr}`);
-            // console.log(`extraAddr=${extraAddr}`);
-
             // 우편번호와 주소 정보를 해당 필드에 넣는다.
-            // document.getElementById('sample3_postcode').value = data.zonecode;
-            // document.getElementById("sample3_address").value = addr;
-            // // 커서를 상세주소 필드로 이동한다.
-            // document.getElementById("sample3_detailAddress").focus();
-               document.getElementById("address").value = addr;
+            // document.getElementById('sample2_postcode').value = data.zonecode;
+             document.getElementById("address").value = addr;
+            // 커서를 상세주소 필드로 이동한다.
+            //document.getElementById("sample2_detailAddress").focus();
+
             // iframe을 넣은 element를 안보이게 한다.
             // (autoClose:false 기능을 이용한다면, 아래 코드를 제거해야 화면에서 사라지지 않는다.)
-            document.getElementById('search_addr_area').style.display = 'none';
-
-            // 우편번호 찾기 화면이 보이기 이전으로 scroll 위치를 되돌린다.
-            document.body.scrollTop = currentScroll;
+            document.getElementById('layer').style.display = 'none';
         },
-        // 우편번호 찾기 화면 크기가 조정되었을때 실행할 코드를 작성하는 부분. iframe을 넣은 element의 높이값을 조정한다.
-        onresize : function(size) {
-            document.getElementById('search_addr_area').style.height = size.height+'px';
-        },
-        // width : '100%',
-        width:'350px',
+        width : '100%',
         height : '100%',
         maxSuggestItems : 5
-    }).embed(document.getElementById('search_addr_area'));
+    }).embed(document.getElementById('layer'));
 
     // iframe을 넣은 element를 보이게 한다.
-    document.getElementById('search_addr_area').style.display = 'block';
+    document.getElementById('layer').style.display = 'block';
+
+    // iframe을 넣은 element의 위치를 화면의 가운데로 이동시킨다.
+    initLayerPosition();
 }
 
-function RegisterAddress(){
+// 브라우저의 크기 변경에 따라 레이어를 가운데로 이동시키고자 하실때에는
+// resize이벤트나, orientationchange이벤트를 이용하여 값이 변경될때마다 아래 함수를 실행 시켜 주시거나,
+// 직접 element_layer의 top,left값을 수정해 주시면 됩니다.
+function initLayerPosition(){
+    var width = 300; //우편번호서비스가 들어갈 element의 width
+    var height = 400; //우편번호서비스가 들어갈 element의 height
+    var borderWidth = 5; //샘플에서 사용하는 border의 두께
 
-    const address_type = document.querySelector("input[name='address-type']:checked");
-    const address_type_value = address_type ? address_type.value : null
-    const address = document.getElementById("address").value;
-    const user_name = window.env.login_user.name;
-
-    if(!user_name){
-        alert('로그인 이후 사용해주세요');
-        return;
-    }
-
-    if(!address_type_value){
-        alert('주소타입을 선택해주세요');
-        return;
-    }
-
-    if(!address){
-        alert('주소를 입력해 주세요');
-        return;
-    }
-
-    const address_json = {
-                            user_name:user_name,
-                            address_type:address_type_value,
-                            address:address
-                         }
-
-    console.log(address_json);                         
+    // 위에서 선언한 값들을 실제 element에 넣는다.
+    document.getElementById('layer').style.width = width + 'px';
+    document.getElementById('layer').style.height = height + 'px';
+    document.getElementById('layer').style.border = borderWidth + 'px solid';
+    // 실행되는 순간의 화면 너비와 높이 값을 가져와서 중앙에 뜰 수 있도록 위치를 계산한다.
+    document.getElementById('layer').style.left = (((window.innerWidth || document.documentElement.clientWidth) - width)/2 - borderWidth) + 'px';
+    document.getElementById('layer').style.top = (((window.innerHeight || document.documentElement.clientHeight) - height)/2 - borderWidth) + 'px';
 }
